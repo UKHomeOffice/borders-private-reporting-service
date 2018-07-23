f1554a2f2eb7c6812801490a3091aff5c7a9cc74
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-07-23 13:19:06 +0100

    fixed for sonar

--- src/services/AuthorizationChecker.js
+++ src/services/AuthorizationChecker.js
@@ -4,16 +4,10 @@ class AuthorizationChecker {
     }
 
     isAuthorized(currentUser, html) {
-
-        const command = html("meta[name='command']").attr("content") ? html("meta[name='command']").attr("content").split(',') : [];
-        const subcommand = html("meta[name='subcommand']").attr("content") ? html("meta[name='subcommand']").attr("content").split(','): [];
-        const team = html("meta[name='team']").attr("content") ? html("meta[name='team']").attr("content").split(',') : [];
-        const location = html("meta[name='location']").attr("content") ? html("meta[name='location']").attr("content").split(','): [];
-
-        if (!this.hasContent(command) && !this.hasContent(subcommand) && !this.hasContent(team) && !this.hasContent(location)) {
+        const {command, subcommand, team, location} = this.details(html);
+        if (this.isToBeSeenByAll({command, subcommand, team, location})) {
             return true;
         }
-
         if (command.filter((c => currentUser['commandid'] === c)).length >= 1) {
             return true;
         } else if (subcommand.filter((c => currentUser['subcommandid'] === c)).length >= 1) {
@@ -25,6 +19,26 @@ class AuthorizationChecker {
 
     hasContent(object) {
         return object.length !== 0;
+    }
+
+    details(html) {
+        const command = html("meta[name='command']").attr("content") ? html("meta[name='command']").attr("content").split(',') : [];
+        const subcommand = html("meta[name='subcommand']").attr("content") ? html("meta[name='subcommand']").attr("content").split(',') : [];
+        const team = html("meta[name='team']").attr("content") ? html("meta[name='team']").attr("content").split(',') : [];
+        const location = html("meta[name='location']").attr("content") ? html("meta[name='location']").attr("content").split(',') : [];
+        return {
+            command,
+            subcommand,
+            team,
+            location
+        }
+    }
+
+    isToBeSeenByAll({command, subcommand, team, location}) {
+      return !this.hasContent(command)
+          && !this.hasContent(subcommand)
+          && !this.hasContent(team)
+          && !this.hasContent(location)
     }
 
 }
