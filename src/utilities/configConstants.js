d1db2b295dbdbc28bd0055911521ecb17cb54c7e
Amin <amin.mohammed-coleman@digital.homeoffice.gov.uk>
2018-11-29 14:27:43 +0000

    removed unused constant

--- src/utilities/configConstants.js
+++ src/utilities/configConstants.js
@@ -1,14 +1,7 @@
 
-
-const port = process.env.PORT || 8080;
-
-const baseUrl = process.env.NODE_ENV === 'production' ? `${process.env.REPORTING_SERVICE_NAME}.${process.env.DOMAIN}`:
-    `localhost:${port}`;
-
 const configConstants = () => {
     return {
         reportsDir: "/workflow-resources/reports",
-        baseUrl: baseUrl,
         platformDataProxyUrl: process.env.PLATFORM_DATA_PROXY_URL
     }
 };
